b94f1f08681623d1b630daf9d37139a8f6e9d47b
vanessa_campos <vanessa.c.oliveira93@gmail.com>
2021-10-04 00:37:06 -0300

    camera ajustes - limite de x final e y negativo

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target = null;
+    [SerializeField] float stopPos;
     [SerializeField] private float followDelay = 4;
 
     private Vector3 offset;
@@ -19,5 +20,11 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * followDelay);         
+        if(transform.position.x > stopPos){
+            transform.position = new Vector3(stopPos, transform.position.y, transform.position.z);
+        }
+        if(target.position.y < -3){
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
     }
 }
