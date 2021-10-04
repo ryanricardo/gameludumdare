6e2e0717cba73893777d67f1a16f7665166a566f
vanessa_campos <vanessa.c.oliveira93@gmail.com>
2021-10-04 13:19:05 -0300

    passar 3 pedras

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -14,13 +14,15 @@ public class Finish : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player") )
-        {
-            gm.levelCompleted = true;
+        {            
             GameObject rocksPlayer0 = GameObject.FindGameObjectWithTag("PedraController 0");
-            GameObject rocksPlayer1 = GameObject.FindGameObjectWithTag("PedraController 1");            
-            Destroy(rocksPlayer0);
-            Destroy(rocksPlayer1);
-            Destroy(other.gameObject);
+            GameObject rocksPlayer1 = GameObject.FindGameObjectWithTag("PedraController 1");   
+            if(rocksPlayer0!= null & rocksPlayer1!=null){                
+                gm.levelCompleted = true;         
+                Destroy(rocksPlayer0);
+                Destroy(rocksPlayer1);
+                Destroy(other.gameObject);
+            }
         }
     }
 }
