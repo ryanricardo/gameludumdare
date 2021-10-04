13a9b116c88572959c8113e5f0c560a934761045
vanessa_campos <vanessa.c.oliveira93@gmail.com>
2021-10-03 23:55:45 -0300

    camera script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target = null;
+    [SerializeField] private float followDelay = 4;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Camera Updates should be done in LateUpdate
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * followDelay);         
+    }
+}
