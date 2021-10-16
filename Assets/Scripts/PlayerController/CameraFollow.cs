a364dd065bf725deeabeeed973ab8aeb105dd9f4
vanessa_campos <vanessa.c.oliveira93@gmail.com>
2021-10-16 18:41:48 -0300

    camera teste ajuste

--- Assets/Scripts/PlayerController/CameraFollow.cs
+++ Assets/Scripts/PlayerController/CameraFollow.cs
@@ -8,6 +8,7 @@ public class CameraFollow : MonoBehaviour
     [Header("Components")]
     [SerializeField]    public      Transform       target = null;
     [HideInInspector]   private     Vector3         offset;
+                        private     Vector3         camStartPos;
 
     [Header("Atributtes Camera")]
     [SerializeField]    private     float           stopPos;
@@ -20,6 +21,7 @@ public class CameraFollow : MonoBehaviour
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
         offset = transform.position - target.position;
+        camStartPos = transform.position;
     }
 
     // Camera Updates should be done in LateUpdate
@@ -27,12 +29,25 @@ public class CameraFollow : MonoBehaviour
     {
         if(target == null)
         return;
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * followDelay);         
-        if(transform.position.x > stopPos){
+
+        if(target.position.x >= camStartPos.x){
+            transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * followDelay);     
+        }
+        if(transform.position.x >= stopPos){
             transform.position = new Vector3(stopPos, transform.position.y, transform.position.z);
         }
-        if(target.position.y < -3){
-            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        if(target.position.x < camStartPos.x){
+            Vector3 newPos = transform.position;
+            newPos.x = camStartPos.x;
+            transform.position = newPos;
         }
+
+        // transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * followDelay);         
+        // if(transform.position.x > stopPos){
+        //     transform.position = new Vector3(stopPos, transform.position.y, transform.position.z);
+        // }
+        // if(target.position.y < -3){
+        //     transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        // }
     }
 }
