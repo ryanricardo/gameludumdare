c6ab3cf3e8c77e24a586eff783e9f416008f600b
Ryan <70291872+ryanricardo@users.noreply.github.com>
2021-10-22 16:50:36 -0300

    Script de Finish aceita ambas pedras do jogador

--- Assets/Scripts/GameManager/Finish.cs
+++ Assets/Scripts/GameManager/Finish.cs
@@ -13,14 +13,17 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("Player") )
+        if(other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("RockController 1")
+        || other.gameObject.CompareTag("RockController 2"))
         {            
             PlayerPrefs.SetInt("ScenesPassed", gm.currentScene);
             GameObject rocksPlayer0 = GameObject.FindGameObjectWithTag("RockController 1");
             GameObject rocksPlayer1 = GameObject.FindGameObjectWithTag("RockController 2");  
+            GameObject player       = GameObject.FindGameObjectWithTag("Player");
             gm.levelCompleted = true;   
             Destroy(rocksPlayer0);
             Destroy(rocksPlayer1);
+            Destroy(player);
             Destroy(other.gameObject);
            // if(rocksPlayer0!= null & rocksPlayer1!=null){                
                        
