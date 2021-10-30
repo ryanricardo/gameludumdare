284efcf3bf1fa3b751e0688f998deeb5788f4010
vanessa_campos <vanessa.c.oliveira93@gmail.com>
2021-10-30 19:05:41 -0300

    nivel 0 fases 1/10 e bug slider

--- Assets/Scripts/Objetos/ResetPosition.cs
+++ Assets/Scripts/Objetos/ResetPosition.cs
@@ -5,26 +5,35 @@ using UnityEngine;
 public class ResetPosition : MonoBehaviour
 {
     [Header("Components")]
-    [HideInInspector]private PlayerController playerController;
-    [HideInInspector]private GameManager      gameManager;
+    // NewPlayerController player;
+    // GameManager gameManager;
+    Vector3 playerPos, rock1Pos, rock2Pos;
+    GameObject player, rock1, rock2;
 
     void Start()
     {
-        gameManager = FindObjectOfType<GameManager>();
-        playerController = FindObjectOfType<PlayerController>();
-    }
-
-    void Update()
-    {
-        
+        // gameManager = FindObjectOfType<GameManager>();
+        // player = FindObjectOfType<NewPlayerController>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        rock1 = GameObject.FindGameObjectWithTag("RockController 1");
+        rock2 = GameObject.FindGameObjectWithTag("RockController 2");
+        playerPos = player.transform.position;
+        rock1Pos = rock1.transform.position;
+        rock2Pos = rock2.transform.position;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         
-        if(other.gameObject.CompareTag("Player"))
-        {
-            gameManager.gameOver = true;
+        if(other.gameObject.CompareTag("Player")){
+            // gameManager.gameOver = true;
+            player.transform.position = playerPos;
+            rock1.transform.position = rock1Pos;
+            rock2.transform.position = rock2Pos;
+        }
+        if(other.gameObject.CompareTag("RockController 1")){
+            rock1.transform.position = rock1Pos;
         }
     }
+
 }
