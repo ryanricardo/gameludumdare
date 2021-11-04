bb08327e8f3ca3142d7fd300d7a487ad4eb6a5af
Ryan <70291872+ryanricardo@users.noreply.github.com>
2021-11-04 16:02:19 -0300

    Verificador de skins

--- Assets/Scripts/Objetos/PowerUps.cs
+++ Assets/Scripts/Objetos/PowerUps.cs
@@ -66,14 +66,35 @@ public class PowerUps : MonoBehaviour
             switch(indexRock)
             {
                 case 1:
+                    for(int i = 1; i < data.skinRock1.Count; i++)
+                    {
+                        if(data.skinsRewardRock1[indexSkinReward] == data.skinRock1[i])
+                        {
+                            return; 
+                        }
+                    }
                     data.skinRock1.Add(data.skinsRewardRock1[indexSkinReward]);
                 break;
 
                 case 2:
+                    for(int i = 1; i < data.skinRock2.Count; i++)
+                    {
+                        if(data.skinsRewardRock2[indexSkinReward] == data.skinRock2[i])
+                        {
+                            return; 
+                        }
+                    }
                     data.skinRock2.Add(data.skinsRewardRock2[indexSkinReward]);
                 break;
 
                 case 3:
+                    for(int i = 1; i < data.skinRock3.Count; i++)
+                    {
+                        if(data.skinsRewardRock3[indexSkinReward] == data.skinRock3[i])
+                        {
+                            return; 
+                        }
+                    }
                     data.skinRock3.Add(data.skinsRewardRock3[indexSkinReward]);
                 break;
             }
