72074cb9434c648fb2ac9067fa2a9ab65bbff179
Ryan <70291872+ryanricardo@users.noreply.github.com>
2021-11-10 09:57:25 -0300

    Botão mute em ambos volumes

--- Assets/OptionsManager.cs
+++ Assets/OptionsManager.cs
@@ -10,10 +10,21 @@ public class OptionsManager : MonoBehaviour
     [Header("Components")]
     [SerializeField]    private Slider              sliderAjustMusicGame;
     [SerializeField]    private Slider              sliderAjustEffectsGame;
+    [SerializeField]    GameObject                  ButtonMuteMusic;
+    [SerializeField]    GameObject                  ButtonMuteEffects;
+    [SerializeField]    private Sprite              spriteMutated;
+    [SerializeField]    private Sprite              spriteUnmuted;
+
+    [Header("Atributtes Volumes")]
+    [HideInInspector]   private bool                muteMusic;
+    [HideInInspector]   private bool                muteEffects;
+    
 
 
     void Start()
     {
+        muteMusic = false;
+        muteEffects = false;
         sliderAjustMusicGame.value = PlayerPrefs.GetFloat("VolumeMusicGame");
         sliderAjustEffectsGame.value = PlayerPrefs.GetFloat("VolumeEffectsGame");
 
@@ -24,4 +35,41 @@ public class OptionsManager : MonoBehaviour
         PlayerPrefs.SetFloat("VolumeMusicGame", sliderAjustMusicGame.value);
         PlayerPrefs.SetFloat("VolumeEffectsGame", sliderAjustEffectsGame.value);        
     }
+
+    public void MuteMusic()
+    {
+        muteMusic ^= true;
+
+        if(muteMusic)
+        {
+            ButtonMuteMusic.GetComponent<Image>().sprite = spriteMutated;
+            sliderAjustMusicGame.value = 0;
+            sliderAjustMusicGame.interactable = false;
+        }else 
+        {
+            ButtonMuteMusic.GetComponent<Image>().sprite = spriteUnmuted;
+            sliderAjustMusicGame.interactable = true;
+            sliderAjustMusicGame.value = PlayerPrefs.GetFloat("VolumeMusicGame");
+        }
+        
+    }
+
+    public void MuteEffects()
+    {
+        muteEffects ^= true;
+
+        if(muteEffects)
+        {
+            ButtonMuteEffects.gameObject.GetComponent<Image>().sprite = spriteMutated;
+            sliderAjustEffectsGame.value = 0;
+            sliderAjustEffectsGame.interactable = false;
+        }else 
+        {
+            ButtonMuteEffects.gameObject.GetComponent<Image>().sprite = spriteUnmuted;
+            sliderAjustEffectsGame.interactable = true;
+            sliderAjustEffectsGame.value = PlayerPrefs.GetFloat("VolumeEffectsGame");
+
+        }
+    }
 }
+
