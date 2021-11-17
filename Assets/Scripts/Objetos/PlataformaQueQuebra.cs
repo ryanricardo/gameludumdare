edc5d6e7d531d74f095c27dbf228bfab7c7f2c4d
PequenoPreto <lucas.matulis@gmail.com>
2021-11-17 14:51:56 -0300

    plataforma q quebra completa

--- Assets/Scripts/Objetos/PlataformaQueQuebra.cs
+++ Assets/Scripts/Objetos/PlataformaQueQuebra.cs
@@ -6,40 +6,62 @@ public class PlataformaQueQuebra : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer SR;
     [SerializeField] private BoxCollider2D BC2;
-    [SerializeField] private bool destruido=false;
+    [SerializeField] private BoxCollider2D BC2T;
+    [SerializeField] private float tempoBase;
+    [SerializeField] private float tempo;
+    [SerializeField] private bool desgaste;
+    
     void Start()
     {
-        
+        tempo=tempoBase;
     }
 
     
     void Update()
     {
-        if(destruido)
+        if(tempo>tempoBase)
         {
-            SR.enabled=false;
-            BC2.enabled=false;
+            tempo=tempoBase;
         }
-        else
+
+        if(desgaste)
         {
-            SR.enabled=true;
+            tempo-=Time.deltaTime;
+        }
+
+        if(!desgaste & tempo<tempoBase)
+        {
+            tempo+=Time.deltaTime;
+        }
+
+        if(tempo<0)
+        {
+            BC2.enabled=false;
+            SR.enabled=false;
+            BC2T.enabled=false;
+        }
+        else if(tempo==tempoBase)
+        {
             BC2.enabled=true;
+            SR.enabled=true;
+            BC2T.enabled=true;
         }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-       if(col.gameObject.tag=="RockController 1" || col.gameObject.tag=="RockController 2" || col.gameObject.tag== "Player"|| col.gameObject.tag=="pedra")
-       {
-           
-       }
+      if(col.gameObject.tag=="RockController 1" || col.gameObject.tag=="RockController 2" || col.gameObject.tag== "Player" || col.gameObject.tag=="pedra")
+      {
+         desgaste=true;
+      }
     }
     
     void OnTriggerExit2D(Collider2D col)
     {
-        if(col.gameObject.tag=="RockController 1" || col.gameObject.tag=="RockController 2" || col.gameObject.tag== "Player"|| col.gameObject.tag=="pedra")
-       {
-
-       }
+        if(col.gameObject.tag=="RockController 1" || col.gameObject.tag=="RockController 2" || col.gameObject.tag== "Player" || col.gameObject.tag=="pedra")
+        {
+            desgaste=false;
+        }
     }
+
 }
