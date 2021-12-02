500245169161a280891baf9b3caf7332cb4fecd1
PequenoPreto <lucas.matulis@gmail.com>
2021-12-02 18:49:36 -0300

    plataforma q quebra de vez

---
+++ Assets/Scripts/Objetos/PlataformaQueQuebraDeVez.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaQueQuebraDeVez : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer SR;
+    [SerializeField] private BoxCollider2D BC2;
+    [SerializeField] private BoxCollider2D BC2T;
+    [SerializeField] private float tempoBase;
+    [SerializeField] private float tempo;
+    [SerializeField] private bool desgaste;
+    
+    void Start()
+    {
+        tempo=tempoBase;
+    }
+
+    
+    void Update()
+    {
+        if(tempo>tempoBase)
+        {
+            tempo=tempoBase;
+        }
+
+        if(desgaste)
+        {
+            tempo-=Time.deltaTime;
+        }
+
+
+        if(tempo<0)
+        {
+            BC2.enabled=false;
+            SR.enabled=false;
+            BC2T.enabled=false;
+        }
+        else if(tempo==tempoBase)
+        {
+            BC2.enabled=true;
+            SR.enabled=true;
+            BC2T.enabled=true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+      if(col.gameObject.tag=="RockController 1" || col.gameObject.tag=="RockController 2" || col.gameObject.tag== "Player" || col.gameObject.tag=="pedra")
+      {
+         desgaste=true;
+      }
+    }
+    
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag=="RockController 1" || col.gameObject.tag=="RockController 2" || col.gameObject.tag== "Player" || col.gameObject.tag=="pedra")
+        {
+            desgaste=false;
+        }
+    }
+}
