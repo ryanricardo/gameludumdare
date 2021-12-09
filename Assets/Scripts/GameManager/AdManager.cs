ce297d64456baea1ce8ead2b368bc603126992cb
PequenoPreto <lucas.matulis@gmail.com>
2021-12-09 16:07:26 -0300

    rascunho do AdManager

---
+++ Assets/Scripts/GameManager/AdManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+   #if UNITY_IOS
+   private string gameid="4493928";
+   #elif UNITY_ANDROID
+   private string gameid="4493929";
+   #endif
+
+   bool testmode=true;
+   
+   void Start()
+   {
+       Advertisement.Initialize(gameid, testmode);
+   }
+
+   public void ShowInterstitialAd()
+   {
+       if(Advertisement.isReady())
+       {
+           Advertisement.Show(gameid);
+       }
+       else
+       {
+           Debug.Log("Não está pronto o anuncio");
+       }
+   }
+
+}
