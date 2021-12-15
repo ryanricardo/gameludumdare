541583de9309b86e00720f97dac95afa87f0a8dc
Ryan <70291872+ryanricardo@users.noreply.github.com>
2021-12-15 18:46:11 -0300

    Som de porta

--- Assets/Scripts/Objetos/Porta.cs
+++ Assets/Scripts/Objetos/Porta.cs
@@ -6,16 +6,21 @@ public class Porta : MonoBehaviour
 {
     public Animator anim;
     public Botao botao;
+    public AudioSource sourcePorta;
+    public AudioClip clipOpen;
+    private bool playClipOneTime;
 
     void Update()
     {
         if(botao.ativo)
         {
             anim.SetTrigger("Abrir");
+            if(playClipOneTime){sourcePorta.PlayOneShot(clipOpen); playClipOneTime = false;}
         }
         else
         {
             anim.SetTrigger("Fechar");
+            playClipOneTime = true;
         }
     }
 }
